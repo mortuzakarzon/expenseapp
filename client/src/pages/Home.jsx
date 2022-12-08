ddc9a641aedece00c304b81534df3da4ed8c9486
Mortuza Karzon <karzonku@gmail.com>
2022-12-08 15:18:15 +0600

    Adeded the transation data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,12 @@ import axios from "axios";
 import { useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import "../resources/transactions.css";
+import { useState } from "react";
+import { Modal } from "antd";
+import { Form, Input, Select } from 'antd';
 
 function Home() {
-
+  const [showEditTransactionModel, setShowEditTransactionModel] = useState(false);
   const getData = async () => {
     try {
       const response = await axios.post("/api/users/get-info-by-id", {}, {
@@ -26,11 +29,13 @@ function Home() {
     <DefaultLayout>
       <div className="filter d-flex justify-content-between align-items-center">
         <div>
-          
+
         </div>
 
         <div>
-          <button className="primary"> ADD NEW </button>
+          <button className="primary addButton" onClick={() => { setShowEditTransactionModel(true) }}>
+            ADD NEW
+          </button>
         </div>
 
       </div>
@@ -38,6 +43,83 @@ function Home() {
       <div className="table-analytics">
 
       </div>
+
+      <Modal title="Add Transaction"
+        visible={showEditTransactionModel}
+        setOpen={setShowEditTransactionModel}
+        onCancel={() => {
+          setShowEditTransactionModel(false)
+        }}
+      >
+
+        <Form layout="vertical">
+          <Form.Item
+            label="Amount"
+            name="amount"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item
+            label="Type"
+            name="type"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          ><Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
+              
+            </Select>
+
+          </Form.Item>
+          <Form.Item
+            label="Category"
+            name="category"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          ><Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="freelance">Freelance</Select.Option>
+              <Select.Option value="otherIncome">Other Income</Select.Option>
+              <Select.Option value="rent">Rent</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="entertainment">Entertainment</Select.Option>
+              <Select.Option value="otherExpense">Other Expense</Select.Option>
+            </Select>
+
+          </Form.Item>
+          
+            <Form.Item
+              label="Date"
+              name="date"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input type="date" />
+            </Form.Item>
+
+            <Form.Item label="References" name="references">
+              <Input type="text" />
+            </Form.Item>
+
+            <Form.Item label="Description" name="description">
+              <Input type="text" />
+            </Form.Item>
+          </Form>
+      </Modal>
 
 
 
